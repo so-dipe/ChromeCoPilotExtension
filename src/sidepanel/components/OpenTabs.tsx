86c0c3c2a91b5adb5d07c5d87053054a8c1da4cb
so-dipe <sodipepaul@gmail.com>
2024-05-09 18:57:12 +0100

    ui changes to chat and components

--- src/sidepanel/components/OpenTabs.tsx
+++ src/sidepanel/components/OpenTabs.tsx
@@ -1,24 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import { readPdfUrl } from '../utils/file_reader';
 import { htmlParser } from '../utils/html_parser';
-import { DocumentsDB } from '../../db/db';
+import { DocumentsDB, ConversationsDB } from '../../db/db';
+import { useUserData } from '../hooks/chromeStorageHooks';
+import 'tailwindcss/tailwind.css';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+interface Props {
+    chatId: string;
+    onSelectTab: (tab: chrome.tabs.Tab) => void;
+}
 
 const extractHTMl = () => { 
     return document.documentElement.outerHTML;
 }
 
-const OpenTabs: React.FC<{ onSelectTab: (tab: chrome.tabs.Tab) => void }> = ({ onSelectTab }) => {
+const OpenTabs: React.FC<Props> = ({ chatId, onSelectTab }) => {
     const [tabs, setTabs] = useState<chrome.tabs.Tab[]>([]);
     const [selectedTab, setSelectedTab] = useState<chrome.tabs.Tab | null>(null);
     const [currentTab, setCurrentTab] = useState<chrome.tabs.Tab | null>(null);
-    const [docsDb, setDocsDb] = useState<any>(null);
+    const [docsDb, setDocsDb] = useState<any>(new DocumentsDB());
+    const [db, setDb] = useState<ConversationsDB | null>(null);
+    const user = useUserData();
 
     useEffect(() => {
-        const db = new DocumentsDB();
-        setDocsDb(db);
-
-        return () => { db.close()}
-    }, [])
+        if (user) {
+            const db = new ConversationsDB(user.localId);
+            setDb(db);
+        }
+    }, [user])
 
     useEffect(() => {
         const handleTabRemove = (tabId: number) => { 
@@ -26,6 +38,7 @@ const OpenTabs: React.FC<{ onSelectTab: (tab: chrome.tabs.Tab) => void }> = ({ o
                 setSelectedTab(null);
             }
             docsDb.deleteDocument(tabId);
+            db && db.removeDocumentFromConversation(chatId, tabId.toString());
         }
         chrome.tabs.onRemoved.addListener(handleTabRemove);
         return () => {
@@ -71,7 +84,9 @@ const OpenTabs: React.FC<{ onSelectTab: (tab: chrome.tabs.Tab) => void }> = ({ o
         if (tab.url && tab.url.split(".").pop() === "pdf") { 
             readPdfUrl(tab.url).then((result) => {
                 docsDb.storeDocument(tabId, tab.title, result);
+                db && db.addDocumentToConversation(chatId, tabId.toString());
             });
+            return;
         }
 
         chrome.scripting.executeScript({ target: { tabId: tabId }, func: extractHTMl },
@@ -80,6 +95,7 @@ const OpenTabs: React.FC<{ onSelectTab: (tab: chrome.tabs.Tab) => void }> = ({ o
                     const htmlContent = result[0].result;
                     const htmlParsed = htmlParser(htmlContent);
                     docsDb.storeDocument(tabId, tab.title, htmlParsed);
+                    db && db.addDocumentToConversation(chatId, tabId.toString());
                 }
             }
         );
@@ -87,20 +103,28 @@ const OpenTabs: React.FC<{ onSelectTab: (tab: chrome.tabs.Tab) => void }> = ({ o
 
     return (
         <div>
-            <h5>Select Tab</h5>
-            <select onChange={(e) => handleTabSelect(e)} value={selectedTab?.id}>
-                {
-                    tabs.map((tab) => (
-                        <option key={tab.id} value={tab.id}>
-                            {tab.id === currentTab?.id ? (
-                                `${tab.title} - Current Tab`
-                            ) : (
-                                tab.title
-                            )}
-                        </option>
-                    ))
-                }
-            </select>
+            <h5 className="text-center font-bold mb-2">Select Tab:</h5>
+            
+            <div className='overflow-hidden'>
+                <select
+                    onChange={(e) => handleTabSelect(e)}
+                    value={selectedTab?.id}
+                    className="w-full py-2 px-4 border border-gray-300 rounded-md text-lg"
+                >
+                    {
+                        tabs.map((tab) => (
+                            <option key={tab.id} value={tab.id} className={tab.id === currentTab?.id ? "font-bold" : ""}>
+                                {tab.id === currentTab?.id ? (
+                                    `${tab.title} - Current Tab`
+                                ) : (
+                                    tab.title
+                                )}
+                            </option>
+                        ))
+                    }
+                </select>
+
+            </div>
             {selectedTab && <h4>Using tab: {selectedTab.title}</h4>}
         </div>
     )
---
+++ src/sidepanel/components/ProfileButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUserData } from '../hooks/chromeStorageHooks';
+import Avatar from '@mui/material/Avatar';
+import "tailwindcss/tailwind.css"; 
+
+const ProfileButton: React.FC = () => {
+    const navigate = useNavigate()
+    const user = useUserData();
+
+    const handleClick = () => {
+        navigate('/profile')
+    }
+    console.log(user)
+
+    return (
+        (
+            user &&
+            (
+                <div onClick={handleClick}>
+                    {user.photoUrl ? <Avatar src={user.photoUrl} /> : <Avatar>{ user.fullName.charAt(0).toUpperCase()}</Avatar>}
+                </div>
+            )
+        )
+    )
+}
+
+export default ProfileButton
